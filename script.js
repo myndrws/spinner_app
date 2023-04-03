505abf3412c2b6384f2079c020295573d2a7dfc3
myndrws <myndrws@gmail.com>
2023-04-03 19:01:56 +0100

    two spinners, second one doesnt work

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 
 
-let container = document.querySelector(".container");
-let btn = document.getElementById("spin");
-let number = Math.ceil(Math.random() * 10000);
-
-let flash = document.querySelector(".container div");
-
+let container1 = document.querySelector("#name_spinner");
+let container2 = document.querySelector("#work_spinner");
+let btn = document.getElementById("spin1");
+let btn2 = document.getElementById("spin2");
+let number1 = Math.ceil(Math.random() * 10000);
+let number2 = Math.ceil(Math.random() * 10000);
 
 btn.onclick = function () {
-  container.style.transform = "rotate(" + number + "deg)";
-  number += Math.ceil(Math.random() * 10000);
+  container1.style.transform = "rotate(" + number1 + "deg)";
+  number1 += Math.ceil(Math.random() * 10000);
+
+}
+
+btn2.onclick = function () {
+  container2.style.transform = "rotate(" + number2 + "deg)";
+  number2 += Math.ceil(Math.random() * 10000);
 
 }
