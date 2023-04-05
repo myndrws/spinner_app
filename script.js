6f5da22fcd0dee167f5d45f7dedeb1fff3aae404
myndrws <myndrws@gmail.com>
2023-04-05 18:17:47 +0100

    needed second js file to get second wheel moving

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
 
+// first script to deal with first wheel
 
 let container1 = document.querySelector("#name_spinner");
-let container2 = document.querySelector("#work_spinner");
-let btn = document.getElementById("spin1");
-let btn2 = document.getElementById("spin2");
-let number1 = Math.ceil(Math.random() * 10000);
-let number2 = Math.ceil(Math.random() * 10000);
+let btn1 = document.getElementById("spin1");
 
-btn.onclick = function () {
+btn1.onclick = function () {
+  let number1 = Math.ceil(Math.random() * 10000);
   container1.style.transform = "rotate(" + number1 + "deg)";
   number1 += Math.ceil(Math.random() * 10000);
-
-}
-
-btn2.onclick = function () {
-  container2.style.transform = "rotate(" + number2 + "deg)";
-  number2 += Math.ceil(Math.random() * 10000);
-
-}
+};
